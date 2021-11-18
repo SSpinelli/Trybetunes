1d64b9966a423565a06e1516aa63fd2062d095c7
joao <jgsspinelli92@gmail.com>
2021-11-18 09:34:28 -0300

    entrega do requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -24,12 +25,20 @@ class Header extends React.Component {
 
   render() {
     const { name, loading } = this.state;
+    const load = (
+      <div>
+        <h2 data-testid="header-user-name">{`Olá ${name}`}</h2>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+      </div>
+    );
     return (
       <header data-testid="header-component">
         <h1>Esse é o header</h1>
         { loading
           ? <Loading />
-          : <h2 data-testid="header-user-name">{`Olá ${name}`}</h2> }
+          : load }
       </header>
     );
   }

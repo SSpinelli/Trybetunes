7a3456f2a671a47ee97316b0b7965b0f53171ef7
joao <jgsspinelli92@gmail.com>
2021-11-18 19:04:25 -0300

    entrega do requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,31 +1,85 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      phrase: '',
+      artist: '',
       buttonStatus: true,
+      albuns: [],
+      loadingAlbuns: false,
+      errorMessage: 'Nenhum álbum foi encontrado',
+      errorStatus: false,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.printAlbum = this.printAlbum.bind(this);
   }
 
   handleChange(type) {
     const typedArtist = type.target.value;
     const minLength = 2;
 
+    this.setState({ artist: typedArtist });
+
     return (typedArtist.length >= minLength)
       ? this.setState({ buttonStatus: false })
       : this.setState({ buttonStatus: true });
   }
 
+  handleClick(click) {
+    click.preventDefault();
+    const { artist } = this.state;
+    this.setState({ loadingAlbuns: true });
+
+    searchAlbumsAPI(artist)
+      .then((response) => {
+        if (response.length > 0) {
+          this.printAlbum(response);
+          return this.setState({ errorStatus: false });
+        } return this.setState({ errorStatus: true });
+      })
+      .then(() => this.setState({ loadingAlbuns: false }));
+    // .catch(() => this.setState({ errorStatus: true }));
+    this.setState({
+      // Fernando Mós me ajudou a resolver o problema do requisito 2;
+      phrase: artist,
+      artist: '',
+    });
+  }
+
+  printAlbum(arrayOfObject) {
+    const albunsList = arrayOfObject.map((album) => (
+      <Link
+        key={ album.collectionId }
+        data-testid={ `link-to-album-${album.collectionId}` }
+        to={ `/album/${album.collectionId}` }
+      >
+        <li>{ album.collectionName }</li>
+      </Link>));
+
+    this.setState({ albuns: albunsList });
+  }
+
   render() {
-    const { buttonStatus } = this.state;
+    const { buttonStatus, albuns, loadingAlbuns,
+      artist, phrase, errorMessage, errorStatus } = this.state;
+    const load = (
+      <div>
+        <h3>{`Resultado de álbuns de: ${phrase}`}</h3>
+        { errorStatus ? <h3>{ errorMessage }</h3> : <ul>{ albuns }</ul> }
+      </div>
+    );
     return (
       <div data-testid="page-search">
-        <h1>Esse é o Search</h1>
-        <form>
+        <form className="hidden">
           <label htmlFor="artist-name">
             <input
+              value={ artist }
               onChange={ this.handleChange }
               type="text"
               id="artist-name"
@@ -36,10 +90,12 @@ class Search extends React.Component {
             type="submit"
             data-testid="search-artist-button"
             disabled={ buttonStatus }
+            onClick={ this.handleClick }
           >
             Procurar
           </button>
         </form>
+        { loadingAlbuns ? <Loading /> : load }
       </div>
     );
   }

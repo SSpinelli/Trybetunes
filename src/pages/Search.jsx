004e1d646f4724b6500bfd2299095cc6d1573e86
joao <jgsspinelli92@gmail.com>
2022-02-11 12:52:10 -0300

    [REFACTORING][STYLE] entrega do requisito 6 e estilizacao da pagina Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import './Style/Search.css';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      artist: '',
       searchedText: '',
       isDisabled: true,
+      discography: [],
+      loading: false,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleClick({ target }) {
-    console.log(target);
+  handleClick(click) {
+    click.preventDefault();
+    const { searchedText } = this.state;
+    this.setState({ loading: true });
+    searchAlbumsAPI(searchedText)
+      .then((albuns) => this.setState({ discography: albuns }))
+      .then(() => this.setState({ loading: false, searchedText: '' }));
+
+    this.setState({ artist: searchedText });
   }
 
   handleChange({ target: { value, name } }) {
@@ -26,29 +39,57 @@ class Search extends React.Component {
   }
 
   render() {
-    const { searchedText, isDisabled } = this.state;
+    const { searchedText, isDisabled, loading, discography, artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form className="search-form">
-          <input
-            className="search-input"
-            data-testid="search-artist-input"
-            onChange={ this.handleChange }
-            name="searchedText"
-            type="text"
-            value={ searchedText }
-          />
-          <button
-            className="search-button"
-            data-testid="search-artist-button"
-            disabled={ isDisabled }
-            onClick={ this.handleClick }
-            type="button"
+        { loading ? <Loading /> : (
+          <form className="search-form">
+            <input
+              className="search-input"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+              name="searchedText"
+              type="text"
+              value={ searchedText }
+            />
+            <button
+              className="search-button"
+              data-testid="search-artist-button"
+              disabled={ isDisabled }
+              onClick={ this.handleClick }
+              type="button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        )}
+        <div className="search-div-h1">
+          <h1>{`Resultado de álbuns de: ${artist}`}</h1>
+        </div>
+        { !discography.length ? (
+          <h1
+            className="search-noResults"
           >
-            Pesquisar
-          </button>
-        </form>
+            Nenhum álbum foi encontrado
+          </h1>
+        ) : (
+          <section className="search-section">
+            { discography && (
+              discography.map((album) => (
+                <Link to={ `/album/${album.collectionId}` } key={ album.collectionId }>
+                  <div
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                    className="search-div-albuns"
+                  >
+                    <h1>{ album.collectionName }</h1>
+                    <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                  </div>
+                </Link>
+              ))
+            )}
+          </section>
+        ) }
       </div>
 
     );
